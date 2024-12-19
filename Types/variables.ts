4fce5dcccb692c69b7572f2cd91cfc517fddcfc5
SarEnderTarkanson <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-12-19 14:06:18 +0100

    some personal basic exercises in TypeScript

---
+++ Types/variables.ts
@@ -0,0 +1,14 @@
+let movieTitle: string = "Se7en";
+
+movieTitle = "Arrival";
+
+movieTitle = 9;
+
+movieTitle.toUpperCase()
+
+let numCatLives: number = 9;
+numCatLives += 1;
+numCatLives = "dog"
+
+let gameOver: boolean = false;
+gameOver = true;
