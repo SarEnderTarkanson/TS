d8be87cb135280b9301a9ef0e66e18c565bdc39c
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-12-22 12:43:13 +0100

    added Fucntions folder and functions.ts

---
+++ Functions/functions.ts
@@ -0,0 +1,11 @@
+// function square(num:number){
+//     return num * num;
+// }
+
+// console.log(square(3))
+
+function greet(person:string){
+    return `Hi there, ${person}!`
+}
+let person = "Alpy"
+console.log(greet(person))
