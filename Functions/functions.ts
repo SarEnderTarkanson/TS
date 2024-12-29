e65139a758cd09a70082be44ecc56e22546c788b
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-12-29 20:54:05 +0100

    experimented a little with the return type annotations

--- Functions/functions.ts
+++ Functions/functions.ts
@@ -1,17 +1,26 @@
-// function square(num:number){
-//     return num * num;
+function square(num: number): number {
+  return num * num;
+}
+
+console.log(square(3));
+
+function greet(person: string): string {
+  return `Hi there, ${person}!`;
+}
+let person = "Alpy";
+console.log(greet(person));
+
+const doSomething = (person: string, age: number, isFunny: boolean) => {};
+
+doSomething("ChickenFace", 39, true);
+
+const add = (x:number, y:number):number => {
+    return x + y;
+}
+
+// function rando(num:number) {
+//     if (Math.random() < 0.5){
+//         return num.toString()
+//     }
+//     return num;
 // }
-
-// console.log(square(3))
-
-function greet(person:string){
-    return `Hi there, ${person}!`
-}
-let person = "Alpy"
-console.log(greet(person))
-
-const doSomething = (person:string, age:number, isFunny:boolean) => {
-
-}
-
-doSomething("ChickenFace", 39, true)
