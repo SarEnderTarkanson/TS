abdf3b6305b2a3f7b524b22caece8057febc8a4f
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-01-11 03:06:49 +0100

    added more exercise on the object types in functions in TypeScript

--- Objects/objects.ts
+++ Objects/objects.ts
@@ -1,5 +1,11 @@
-function printName(person: { first: string, last: string }): void {
-    console.log(`${person.first} ${person.last}`)
+function printName(person: { first: string; last: string }): void {
+  console.log(`${person.first} ${person.last}`);
 }
 
-printName({first: "Alpy", last: "Tarkanson"})
+printName({ first: "Alpy", last: "Tarkanson" });
+
+let coordinate: { x: number; y: number } = { x: 36, y: 42 };
+
+function randomCoordinate(): { x: number; y: number } {
+  return { x: Math.random(), y: Math.random() };
+}
