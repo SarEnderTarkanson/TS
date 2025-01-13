c4dc9a849b8cc9df58b6eab4a65d972df87a3226
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-01-13 03:16:14 +0100

    practice on excess properties

--- Objects/objects.ts
+++ Objects/objects.ts
@@ -9,3 +9,11 @@ let coordinate: { x: number; y: number } = { x: 36, y: 42 };
 function randomCoordinate(): { x: number; y: number } {
   return { x: Math.random(), y: Math.random() };
 }
+
+printName({ first: "Mick", last: "Jagger" }); //TypeScript displaying no error, as expected
+
+printName({ first: "Mick", last: "Jagger", age: 47 }); // TypeScript displaying error, again as expected
+
+const singer = { first: "Mick", last: "Jagger", age: 47 };
+
+printName(singer); //TypeScript not displaying any error, which is unexpected, cos it actually should
