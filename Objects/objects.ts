35ecd16fc17a9afbf56a33896db1c4353c8b281b
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-01-15 02:52:15 +0100

    practice on type aliases

--- Objects/objects.ts
+++ Objects/objects.ts
@@ -1,19 +1,32 @@
-function printName(person: { first: string; last: string }): void {
-  console.log(`${person.first} ${person.last}`);
+// function printName(person: { first: string; last: string }): void {
+//   console.log(`${person.first} ${person.last}`);
+// }
+
+// printName({ first: "Alpy", last: "Tarkanson" });
+
+// let coordinate: { x: number; y: number } = { x: 36, y: 42 };
+
+// function randomCoordinate(): { x: number; y: number } {
+//   return { x: Math.random(), y: Math.random() };
+// }
+
+// printName({ first: "Mick", last: "Jagger" }); //TypeScript displaying no error, as expected
+
+// printName({ first: "Mick", last: "Jagger", age: 47 }); // TypeScript displaying error, again as expected
+
+// const singer = { first: "Mick", last: "Jagger", age: 47 };
+
+// printName(singer); //TypeScript not displaying any error, which is unexpected, cos it actually should
+
+type Point= {
+  x:number;
+  y: number;
 }
 
-printName({ first: "Alpy", last: "Tarkanson" });
+let coordinate: Point = { x: 36, y: 42 };
 
-let coordinate: { x: number; y: number } = { x: 36, y: 42 };
-
-function randomCoordinate(): { x: number; y: number } {
+function randomCoordinate(): Point {
   return { x: Math.random(), y: Math.random() };
 }
 
-printName({ first: "Mick", last: "Jagger" }); //TypeScript displaying no error, as expected
-
-printName({ first: "Mick", last: "Jagger", age: 47 }); // TypeScript displaying error, again as expected
-
-const singer = { first: "Mick", last: "Jagger", age: 47 };
-
-printName(singer); //TypeScript not displaying any error, which is unexpected, cos it actually should
+randomCoordinate()
