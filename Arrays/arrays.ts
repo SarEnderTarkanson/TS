a3046339db43f693f9ed1d10b90fde4a40655410
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-01-20 01:47:58 +0100

    practice for custom types

--- Arrays/arrays.ts
+++ Arrays/arrays.ts
@@ -3,3 +3,17 @@ const activeUsers: string[] = [];
 activeUsers.push("Alpy");
 
 const ageList: number[] = [10, 15, 20];
+
+const bools: Array<boolean> = [true, false];
+
+//declaring custom types
+
+type Point2 = {
+  x: number;
+  y: number;
+};
+
+const coords: Point2[] = [];
+
+coords.push({ x: 23, y: 28 });
+coords.push({ x: 23, y: 28 });
