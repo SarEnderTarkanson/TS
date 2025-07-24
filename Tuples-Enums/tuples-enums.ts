ab1af9ba820c148f7462cad9795729dac25762f9
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-07-24 23:07:54 +0200

    a bit more on tuples

--- Tuples-Enums/tuples-enums.ts
+++ Tuples-Enums/tuples-enums.ts
@@ -3,3 +3,14 @@
 // const color: number[] = [12, 34, 56, 78];
 
 const color: [number, number, number] = [1, 2, 3];
+
+type HTTPResponse = [number, string];
+
+const goodRes: HTTPResponse = [200, "OK"];
+
+goodRes[0] = 100;
+
+const responses: HTTPResponse[] = [
+  [404, "NotFound"],
+  [401, "UnAuthorized"],
+];
